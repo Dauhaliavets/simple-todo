48971a3044a1bb83e8a73801bc904abb5245f75c
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2021-11-14 11:52:46 +0300

    refactor: refac code

--- js/main.js
+++ js/main.js
@@ -20,27 +20,19 @@ const list = [
 ]
 
 function addTask(nameTask, status, priority) {
-
     let idList = list.map(item => item.id);
+    let uniqueId = getUniqueRandomId(1, 100, idList);
+    list.push({id: uniqueId, name: nameTask, status, priority});
+}
 
-    function getUniqueRandomId(minId, maxId, existId) {
-        let randomNumberInRange = Math.floor(Math.random() * (maxId - minId) + minId);
-        return existId.includes(randomNumberInRange) ? getUniqueRandomId(minId, maxId, existId) : randomNumberInRange;
-    }
-
-    let uniqueId = getUniqueRandomId(1, 20, idList);
-
-    list.push({id: uniqueId, name: nameTask, status: status, priority: priority});
-
+function getUniqueRandomId(minId, maxId, existId) {
+    let randomNumberInRange = Math.floor(Math.random() * (maxId - minId) + minId);
+    return existId.includes(randomNumberInRange) ? getUniqueRandomId(minId, maxId, existId) : randomNumberInRange;
 }
 
 function deleteTask(nameTask) {
-
     let index = list.findIndex((item) => item.name === nameTask);
-
-    return (index >= 0) && list.splice(index, 1);
-
-    // return list.filter(item => item.name !== nameTask);
+    return (index !== -1) && list.splice(index, 1);
 }
 
 function changeStatus(nameTask, newStatus) {
@@ -52,52 +44,32 @@ function changeStatus(nameTask, newStatus) {
 };
 
 function showList(targetGroup = 'status') {
-
-    const sortList = list.sort((a, b) => {
+    let sortList = list.sort((a, b) => {
         // Меняем местами a и b, чтобы отсортировать массив по убыванию названий статуса To Do -> In Progress -> Done
         if (targetGroup === 'status') {
             [a, b] = [b, a];
         }
-        if (a[targetGroup] > b[targetGroup]) {
-            return 1;
-        }
-        if (a[targetGroup] < b[targetGroup]) {
-            return -1;
-        }
+        return (a[targetGroup] > b[targetGroup]) ? 1 : -1;
     })
 
-    const mappedList = sortList.map(function (elem, index, array) {
-        if (index > 0 && elem[targetGroup] === array[index - 1][targetGroup]) {
-            return `\n\t${elem.name}`;
-        } else {
-            return `\n${elem[targetGroup]}:\n\t${elem.name}`;
-        }
-    });
+    let output = sortList.map((elem, index, array) => {
+        return (index > 0 && (elem[targetGroup] === array[index - 1][targetGroup]))
+            ? `\n\t${elem.name}`
+            : `\n${elem[targetGroup]}:\n\t${elem.name}`
+        }).join().trim();
 
-    return console.log(mappedList.join().trim());
-
+    console.log(output);
 }
 
-
-console.log('=================== addTask(\'test 6\', \'Done\', \'low\') ======================================');
-addTask('test 6', 'Done', 'low');
-showList('status');
-console.log('=================== addTask(\'sport\', \'Done\', \'low\') ======================================');
-addTask('sport', 'Done', 'low');
-showList('status');
-console.log('=================== changeStatus(\'sport\', \'In Progress\') ======================================');
+addTask('test 6', 'To Do', 'low');
+addTask('sport', 'To Do', 'high');
 changeStatus('sport', 'In Progress');
-showList('status');
-console.log('=================== deleteTask(\'test 6\') ======================================');
 deleteTask('test 6');
-showList('status');
-console.log('=================== addTask(\'sport\', \'Done\', \'low\') ======================================');
-addTask('driving', 'Done', 'low');
-showList('status');
+addTask('driving', 'Done', 'high');
 
-console.log('=================== showList(\'priority\') ======================================');
+console.log('=================== showList( \'priority\' ) ======================================');
 showList('priority');
-console.log('=================== showList(\'status\') ======================================');
+console.log('=================== showList( \'status\' ) ======================================');
 showList('status');
-console.log('=================== showList(\'id\') ======================================');
+console.log('=================== showList( \'id\' ) ======================================');
 showList('id');
